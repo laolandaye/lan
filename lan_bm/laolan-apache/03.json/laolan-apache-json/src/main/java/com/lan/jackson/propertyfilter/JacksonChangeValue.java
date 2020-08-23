d7c9eb8f01ffd42de607eeea176fe5eba85e5330
lan <2476955538@qq.com>
2020-08-23 17:06:59 +0800

    json

--- lan_bm/laolan-apache/03.json/laolan-apache-json/src/main/java/com/lan/jackson/propertyfilter/JacksonChangeValue.java
+++ lan_bm/laolan-apache/03.json/laolan-apache-json/src/main/java/com/lan/jackson/propertyfilter/JacksonChangeValue.java
@@ -1,14 +1,14 @@
 package com.lan.jackson.propertyfilter;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonParser;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.JsonStreamContext;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.databind.cfg.MapperConfig;
 import com.fasterxml.jackson.databind.introspect.AnnotatedField;
 import com.fasterxml.jackson.databind.introspect.AnnotatedMethod;
@@ -25,16 +25,19 @@ import java.util.Map;
 
 public class JacksonChangeValue {
 
-    public static void main(String[] args) {
-        Object returenObj = "{\"msg\":\"success\",\"code\":\"0000\",\"data\":[{\"data\":[{\"address\":\"000\",\"id\":\"000\"}],\"groupId\":1,\"sttr\":\"1\",\"title\":\"“回天有我”出实招，民主议事解难题！\",\"type\":3.0},{\"data\":[{\"address\":\"000\",\"id\":\"000\"}],\"groupId\":2,\"sttr\":\"2\",\"title\":\"“回天有我”出实招，民主议事解难题！\",\"type\":3.0},{\"data\":[{\"address\":\"000\",\"id\":\"000\"}],\"groupId\":3,\"sttr\":\"3\",\"title\":\"“回天有我”出实招，民主议事解难题！\",\"type\":3.0}]}";
-
-        returenObj= JSONObject.fromObject(returenObj);
+    public static void main(String[] args) throws JsonProcessingException {
+//        Object returenObj = "{\"msg\":\"success\",\"code\":\"0000\",\"data\":[{\"data\":[{\"address\":null,\"id\":\"000\"}],\"groupId\":1,\"sttr\":\"1\",\"title\":\"“回天有我”出实招，民主议事解难题！\",\"type\":3.0},{\"data\":[{\"address\":\"000\",\"id\":\"000\"}],\"groupId\":2,\"sttr\":\"2\",\"title\":\"“回天有我”出实招，民主议事解难题！\",\"type\":3.0},{\"data\":[{\"address\":\"000\",\"id\":\"000\"}],\"groupId\":3,\"sttr\":\"3\",\"title\":\"“回天有我”出实招，民主议事解难题！\",\"type\":3.0}]}";
+//        returenObj= JSON.parseObject((String) returenObj);
+        // 数组
+        Object returenObj = "[{\"name\":null,\"url\":\"www.runoob.com\"},{\"name\":\"google\",\"url\":\"www.google.com\"},{\"name\":\"微博\",\"url\":\"www.weibo.com\"}]";
+        returenObj= JSON.parseArray((String) returenObj);
 
         ObjectMapper mapper = new ObjectMapper();
         mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
         mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
         mapper.configure(JsonParser.Feature.ALLOW_COMMENTS, true);
         mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_CONTROL_CHARS, true);
+//        mapper.configure(SerializationFeature.WRITE_NULL_MAP_VALUES, true);
 
         mapper.setVisibility(PropertyAccessor.FIELD, Visibility.ANY);
         mapper.addMixIn(Object.class, PropertyFilterMixIn.class);
